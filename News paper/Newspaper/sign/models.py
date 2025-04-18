525f699f9c04f914b0561f26d241560575d013c5
d1gl03 <arsenijgenij1234@gmail.com>
2025-04-18 15:14:46 +0300

    News_project

--- News paper/Newspaper/sign/models.py
+++ News paper/Newspaper/sign/models.py
@@ -1,9 +1,7 @@
-from django.db import models
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-from allauth.account.forms import SignupForm
-from django.contrib.auth.models import Group
+
 
 class BaseRegisterForm(UserCreationForm):
     email = forms.EmailField(label = "Email")
@@ -18,12 +16,3 @@ class BaseRegisterForm(UserCreationForm):
                   "email",
                   "password1",
                   "password2", )
-
-
-class BasicSignupForm(SignupForm):
-
-    def save(self, request):
-        user = super(BasicSignupForm, self).save(request)
-        common_group = Group.objects.get(name='common')
-        common_group.user_set.add(user)
-        return user
