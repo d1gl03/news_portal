f95575d1e0b3cb91083358d5b773c5aadd3a5159
d1gl03 <arsenijgenij1234@gmail.com>
2025-05-09 00:36:17 +0300

    News_project

--- News paper/Newspaper/Newspaper/settings.py
+++ News paper/Newspaper/Newspaper/settings.py
@@ -59,7 +59,6 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
