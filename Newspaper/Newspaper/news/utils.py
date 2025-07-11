e8c60b269eedebf788c2d3ddecf2bf326cbfc12b
d1gl03 <arsenijgenij1234@gmail.com>
2025-07-11 11:34:12 +0300

    News_project

--- Newspaper/Newspaper/news/utils.py
+++
@@ -1,6 +0,0 @@
-from django.core.mail import send_mail
-from django.conf import settings
-
-def send_notification_email(subject, message, recipient_list):
-    from_email = 'newsportal121@yandex.ru'
-    send_mail(subject, message, from_email, recipient_list)
